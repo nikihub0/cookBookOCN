a3675f9fb2f28a4ba6f7d1ee4d0f20248f25afef
Christian Moskill <cborek@web.de>
2021-09-09 22:26:39 +0200

    Now the shit is working though!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const falafel = document.getElementById('falafel');
 const chorizo = document.getElementById('chorizo');
 const masala = document.getElementById('masala');
+const cheeseCake = document.getElementById('cheeseCake');
+const borscht = document.getElementById('borscht');
+const cupcake = document.getElementById('cupcake');
+const crumble = document.getElementById('crumble');
+const pistachio = document.getElementById('pistachio');
 
 const toggleInfo = (param) => {
     switch (param) {
@@ -12,6 +17,21 @@ const toggleInfo = (param) => {
             break;
         case 'masala':
             masala.style.display === 'block' ? masala.style.display = 'none' : masala.style.display = 'block';
+            break;
+        case 'cheeseCake':
+            cheeseCake.style.display === 'block' ? cheeseCake.style.display = 'none' : cheeseCake.style.display = 'block';
+            break;
+        case 'borscht':
+            borscht.style.display === 'block' ? borscht.style.display = 'none' : borscht.style.display = 'block';
+            break;
+        case 'cupcake':
+            cupcake.style.display === 'block' ? cupcake.style.display = 'none' : cupcake.style.display = 'block';
+            break;
+        case 'crumble':
+            crumble.style.display === 'block' ? crumble.style.display = 'none' : crumble.style.display = 'block';
+            break;
+        case 'pistachio':
+            pistachio.style.display === 'block' ? pistachio.style.display = 'none' : pistachio.style.display = 'block';
             break;
     }
 }
